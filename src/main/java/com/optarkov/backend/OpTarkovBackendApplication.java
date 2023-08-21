0c006628928b2c1f6d2478f800d57b133187d281
EzrealC <ezrealcu@outlook.com>
2023-08-21 22:32:26 +0800

    initial commit

---
+++ src/main/java/com/optarkov/backend/OpTarkovBackendApplication.java
@@ -0,0 +1,13 @@
+package com.optarkov.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpTarkovBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpTarkovBackendApplication.class, args);
+    }
+
+}
